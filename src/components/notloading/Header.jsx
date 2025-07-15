c60733a0d5c29d3ea4fef08bf46a4a61dabf7cd9
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-15 19:05:04 +0530

    Header Completed

--- src/components/notloading/Header.jsx
+++ src/components/notloading/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { SlCalender } from "react-icons/sl";
+import { MdAlbum } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Header = ({ data }) => {
   return (
@@ -13,7 +15,20 @@ const Header = ({ data }) => {
       }}
       className="w-[80vw] h-[60vh] flex flex-col justify-end p-10"
     >
-      
+      <h1 className="text-5xl font-semibold w-[70%] mb-2 text-[#ffffff95]">
+        {data.title || data.name}
+      </h1>
+      <p className="text-sm w-[60%] text-[#ffffff60]">{data.overview}</p>
+      <div className="flex flex-row gap-2 text-[#ffffff80]">
+        <h1 className="flex flex-row items-center justify-center gap-2">
+          <SlCalender className="text-[#ffd700]" /> {data.release_date || "No Information"}
+        </h1>
+        <h1 className="flex flex-row items-center justify-center gap-2">
+          <MdAlbum className="text-xl text-[#ffd700]" />{" "}
+          {data.media_type.toUpperCase()}
+        </h1>
+      </div>
+      <Link className="px-4 py-3 bg-[#ffd700] w-29 rounded-lg text-black mt-2 text-sm font-semibold">Watch Trailer</Link>
     </div>
   );
 };
