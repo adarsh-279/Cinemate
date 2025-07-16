eb659471ffa5ff8c76c8939627bde40780da1bd3
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-16 19:10:26 +0530

    Movie Trending - Trending Page

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import TopNav from "./notloading/TopNav";
+import axios from "../utils/Axios";
 
 const Trending = () => {
-  return (
-    <div>Trending</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Trending
+  const [movie, setMovie] = useState([]);
+  const [tv, setTV] = useState([]);
+
+  const trendingMovies = async () => {
+    try {
+      const { data } = await axios.get(`/trending/movie/day`);
+      setMovie(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const trendingTV = async () => {
+    try {
+      const { data } = await axios.get(`/trending/tv/day`);
+      setTV(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    trendingMovies();
+    trendingTV();
+  }, []);
+
+  return (
+    <div className="w-full h-full min-h-screen p-10 overflow-auto overflow-x-hidden">
+      <div className="w-full flex items-center justify-evenly pb-10">
+        <FaArrowLeftLong
+          onClick={() => {
+            navigate(-1);
+          }}
+          className="text-6xl"
+        />
+        <h1 className="text-5xl opacity-90 ml-[5%] font-semibold">Trending</h1>
+        <TopNav className="-ml-45.5" />
+      </div>
+      <div>
+        <h1 className="text-3xl mt-5 mb-5 font-regular opacity-85">
+          Trending Movies
+        </h1>
+        <div className="flex flex-row flex-wrap ml-4 gap-6 justify-start">
+          {movie.map((d, i) => (
+            <div key={i} className="w-45 h-60 bg-[#00000040] rounded-2xl">
+              <img
+                className="w-35 h-35 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
+                src={`https://image.tmdb.org/t/p/original/${
+                  d.backdrop_path || d.poster_path
+                }`}
+                alt=""
+              />
+              <h1 className="text-lg w-[85%] font-semibold mx-auto mt-4 text-center text-[#ffffff95]">
+                {d.title || d.name}
+              </h1>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
