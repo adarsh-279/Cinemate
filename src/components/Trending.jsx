069bae40795bdc372213af0ba2d9ec7159b84273
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-16 20:57:27 +0530

    Infinite movie scroll - Trending page

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -4,37 +4,47 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 import TopNav from "./notloading/TopNav";
 import axios from "../utils/Axios";
 import Loader from "./Loader";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Trending = () => {
   const navigate = useNavigate();
 
-  const [movie, setMovie] = useState([]);
-  const [tv, setTV] = useState([]);
+    const [trending, setTrending] = useState([]);
+    const [page, setPage] = useState(1)
+    const [hasMore, sethasMore] = useState(true)
 
-  const trendingMovies = async () => {
+  const trendingShow = async () => {
     try {
-      const { data } = await axios.get(`/trending/all/week`);
-      setMovie(data.results);
+        const { data } = await axios.get(`/trending/all/week?page=${page}`);
+        if (data.results.length > 0) {
+            setTrending((prev) => [...prev, ...data.results]);
+            setPage(page+1) 
+        } else {
+            sethasMore(false)
+        }
     } catch (error) {
       console.log(error);
     }
   };
 
-  const trendingTV = async () => {
-    try {
-      const { data } = await axios.get(`/trending/all/week`);
-      setTV(data.results);
-    } catch (error) {
-      console.log(error);
+    const refreshHandler = () => {
+        if (trending.length === 0) {
+            trendingShow();
+        } else {
+            setPage(1);
+            setTrending([]);
+        }
     }
-  };
-
+    
   useEffect(() => {
-    trendingMovies();
-    trendingTV();
+    refreshHandler();
   }, []);
 
-  return movie && tv ? (
+  return trending.length > 0 ? ( < InfiniteScroll
+    dataLength = { trending.length } 
+  next={trendingShow}
+  hasMore={hasMore}
+    loader = {<Loader />} >
     <div className="w-full h-full min-h-screen p-10 overflow-auto overflow-x-hidden">
       <div className="w-full flex items-center justify-evenly pb-10">
         <FaArrowLeftLong
@@ -46,15 +56,11 @@ const Trending = () => {
         <h1 className="text-5xl opacity-90 ml-[5%] font-semibold">Trending</h1>
         <TopNav className="-ml-45.5" />
       </div>
-      <div className="border-b-2 pb-10 border-[#ffffff40]">
-        <h1 className="text-3xl mt-5 mb-5 font-regular opacity-85">
-          Trending Movies
-        </h1>
         <div className="flex flex-row flex-wrap ml-4 gap-6 justify-start">
-          {movie.map((d, i) => (
-            <div key={i} className="w-45 h-60 bg-[#00000040] rounded-2xl">
+          {trending.map((d, i) => (
+            <div key={i} className="w-65 h-92 bg-[#00000040] rounded-2xl">
               <img
-                className="w-35 h-35 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
+                className="w-55 h-70 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
                 src={`https://image.tmdb.org/t/p/original/${
                   d.backdrop_path || d.poster_path
                 }`}
@@ -66,30 +72,8 @@ const Trending = () => {
             </div>
           ))}
         </div>
-      </div>
-      <div className="mt-10">
-        <h1 className="text-3xl mt-5 mb-5 font-regular opacity-85">
-          Trending TV Shows
-        </h1>
-        <div className="flex flex-row flex-wrap ml-4 gap-6 justify-start">
-          {tv.map((d, i) => (
-            <div key={i} className="w-45 h-60 bg-[#00000040] rounded-2xl">
-              <img
-                className="w-35 h-35 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
-                src={`https://image.tmdb.org/t/p/original/${
-                  d.backdrop_path || d.poster_path
-                }`}
-                alt=""
-              />
-              <h1 className="text-lg w-[85%] font-semibold mx-auto mt-4 text-center text-[#ffffff95]">
-                {d.title || d.name}
-              </h1>
-            </div>
-          ))}
-        </div>
-      </div>
     </div>
-  ) : <Loader />
+  </InfiniteScroll> ) : (<Loader />)
 };
 
 export default Trending;
