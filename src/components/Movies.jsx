530eb534363bb21f62d565e2c7923c0fbc13911d
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-17 18:55:58 +0530

    Added ratings

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -57,9 +57,7 @@ const Movie = () => {
             }}
             className="text-6xl"
           />
-          <h1 className="text-5xl opacity-90 ml-[5%] font-semibold">
-            Movies
-          </h1>
+          <h1 className="text-5xl opacity-90 ml-[5%] font-semibold">Movies</h1>
           <TopNav className="-ml-45.5" />
         </div>
         <div className="flex flex-row flex-wrap ml-4 gap-6 justify-start">
@@ -75,6 +73,11 @@ const Movie = () => {
               <h1 className="text-lg w-[85%] font-semibold mx-auto mt-4 text-center text-[#ffffff95]">
                 {d.title || d.name}
               </h1>
+              <div className="w-10 h-10 rounded-full bg-[#ffd700] relative flex items-center justify-center -top-[30%]">
+                <h1 className="text-[#000000] font-bold absolute">
+                  {Math.round((d.vote_average / 10) * 100)}%
+                </h1>
+              </div>
             </div>
           ))}
         </div>
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -42,40 +42,52 @@ const Trending = () => {
     refreshHandler();
   }, []);
 
-  return trending.length > 0 ? ( < InfiniteScroll
-    dataLength = { trending.length } 
-  next={trendingShow}
-  hasMore={hasMore}
-    loader = {<Loader />} >
-    <div className="w-full h-full min-h-screen p-10 overflow-auto overflow-x-hidden">
-      <div className="w-full flex items-center justify-evenly pb-10">
-        <FaArrowLeftLong
-          onClick={() => {
-            navigate(-1);
-          }}
-          className="text-6xl"
-        />
-        <h1 className="text-5xl opacity-90 ml-[5%] font-semibold">Trending</h1>
-        <TopNav className="-ml-45.5" />
-      </div>
+  return trending.length > 0 ? (
+    <InfiniteScroll
+      dataLength={trending.length}
+      next={trendingShow}
+      hasMore={hasMore}
+      loader={<Loader />}
+    >
+      <div className="w-full h-full min-h-screen p-10 overflow-auto overflow-x-hidden">
+        <div className="w-full flex items-center justify-evenly pb-10">
+          <FaArrowLeftLong
+            onClick={() => {
+              navigate(-1);
+            }}
+            className="text-6xl"
+          />
+          <h1 className="text-5xl opacity-90 ml-[5%] font-semibold">
+            Trending
+          </h1>
+          <TopNav className="-ml-45.5" />
+        </div>
         <div className="flex flex-row flex-wrap ml-4 gap-6 justify-start">
           {trending.map((d, i) => (
-            <div key={i} className="w-65 h-92 bg-[#00000040] rounded-2xl">
-              <img
-                className="w-55 h-70 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
-                src={`https://image.tmdb.org/t/p/original/${
-                  d.backdrop_path || d.poster_path
-                }`}
-                alt=""
-              />
-              <h1 className="text-lg w-[85%] font-semibold mx-auto mt-4 text-center text-[#ffffff95]">
-                {d.title || d.name}
-              </h1>
-            </div>
+              <div key={i} className="w-65 h-92 bg-[#00000040] rounded-2xl">
+                <img
+                  className="w-55 h-70 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
+                  src={`https://image.tmdb.org/t/p/original/${
+                    d.backdrop_path || d.poster_path
+                  }`}
+                  alt=""
+                />
+                <h1 className="text-lg w-[85%] font-semibold mx-auto mt-4 text-center text-[#ffffff95]">
+                  {d.title || d.name}
+                </h1>
+              <div className="w-10 h-10 rounded-full bg-[#ffd700] relative flex items-center justify-center -top-[30%]">
+                <h1 className="text-[#000000] font-bold absolute">
+                  {Math.round((d.vote_average / 10) * 100)}%
+                </h1>
+              </div>
+              </div>
           ))}
         </div>
-    </div>
-  </InfiniteScroll> ) : (<Loader />)
+      </div>
+    </InfiniteScroll>
+  ) : (
+    <Loader />
+  );
 };
 
 export default Trending;
