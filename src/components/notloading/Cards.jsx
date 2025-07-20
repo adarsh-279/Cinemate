cf732c2df8c51878792cb88a741c5e4ffe097c7c
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-20 17:58:53 +0530

    Cards Routing - Movies & Tv Shows

--- src/components/notloading/Cards.jsx
+++ src/components/notloading/Cards.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Cards = ({ movieData, tvData }) => {
   return (
@@ -10,18 +11,23 @@ const Cards = ({ movieData, tvData }) => {
       </div>
       <div className="w-[100%] h-full p-2 gap-5 flex flex-row flex-nowrap overflow-x-auto">
         {movieData.map((d, i) => (
-          <div key={i} className="min-w-50 h-70 bg-[#00000040] rounded-lg">
-            <img
-              className="w-45 h-45 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
-              src={`https://image.tmdb.org/t/p/original/${
-                d.backdrop_path || d.poster_path
-              }`}
-              alt=""
-            />
-            <h1 className="text-lg w-[85%] font-semibold mx-auto mt-2 text-center text-[#ffffff95]">
-              {d.title || d.name}
-            </h1>
-          </div>
+          <Link key={i} to={`/movies/details/${d.id}`}>
+            <div
+              key={i}
+              className="min-w-50 h-70 flex flex-col justify-start bg-[#00000040] rounded-xl"
+            >
+              <img
+                className="w-40 h-40 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
+                src={`https://image.tmdb.org/t/p/original/${
+                  d.backdrop_path || d.poster_path
+                }`}
+                alt=""
+              />
+              <h1 className="text-lg w-[85%] font-semibold mx-auto pt-2 text-center text-[#ffffff95]">
+                {d.title || d.name}
+              </h1>
+            </div>
+          </Link>
         ))}
       </div>
 
@@ -32,18 +38,23 @@ const Cards = ({ movieData, tvData }) => {
       </div>
       <div className="w-[100%] h-full p-2 gap-5 flex flex-row flex-nowrap overflow-x-auto">
         {tvData.map((d, i) => (
-          <div key={i} className="min-w-50 h-70 bg-[#00000040] rounded-lg">
-            <img
-              className="w-45 h-45 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
-              src={`https://image.tmdb.org/t/p/original/${
-                d.backdrop_path || d.poster_path
-              }`}
-              alt=""
-            />
-            <h1 className="text-lg w-[85%] font-semibold mx-auto mt-2 text-center text-[#ffffff95]">
-              {d.title || d.name}
-            </h1>
-          </div>
+          <Link key={i} to={`/tvshows/details/${d.id}`}>
+            <div
+              key={i}
+              className="min-w-50 h-70 flex flex-col justify-start bg-[#00000040] rounded-lg"
+            >
+              <img
+                className="w-40 h-40 mt-4 rounded-xl mx-auto overflow-hidden object-cover"
+                src={`https://image.tmdb.org/t/p/original/${
+                  d.backdrop_path || d.poster_path
+                }`}
+                alt=""
+              />
+              <h1 className="text-lg w-[85%] font-semibold mx-auto pt-2 text-center text-[#ffffff95]">
+                {d.title || d.name}
+              </h1>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
