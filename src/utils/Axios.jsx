7771a40719b294e72b638e578c6323578184ae12
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-21 11:49:55 +0530

    Updated - server issues

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -1,10 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: import.meta.env.VITE_TMDB_API_KEY,
+  },
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
   },
 });
 
